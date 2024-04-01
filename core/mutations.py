5b45084a064e5705fb95a7e605d272d071cb2a6d
vvgoutham2019 <vvgoutham2019@gmail.com>
2024-04-01 10:31:46 +0530

    Update post mutation

--- core/mutations.py
+++ core/mutations.py
@@ -1,9 +1,13 @@
 import graphene
 
 from categories.mutations import CreateCategory, DeleteCategory, UpdateCategory
+from posts.mutations import CreatePost, UpdatePost
 
 
 class Mutation(graphene.ObjectType):
     create_category = CreateCategory.Field()
     update_category = UpdateCategory.Field()
     delete_category = DeleteCategory.Field()
+    create_post = CreatePost.Field()
+    update_post = UpdatePost.Field()
+    
---
+++ posts/mutations.py
@@ -0,0 +1,46 @@
+import graphene
+from graphql import GraphQLError
+
+from posts.models import Post
+from posts.forms import PostForm
+
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        content = graphene.String(required=True)
+        category_id = graphene.Int(required=True)
+        
+    success = graphene.Boolean()
+    
+    def mutate(self, info, title, content, category_id, **kwargs):
+        form = PostForm({"title": title, "content": content, "category": category_id})
+        if form.is_valid():
+            new_post = form.save()
+            return CreatePost(success=True)
+        print(form.errors)
+        return CreatePost(success=True)
+    
+class UpdatePost(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required = True)
+        title = graphene.String(required = True)
+        content = graphene.String(required = True)
+        category_id = graphene.Int(required = True)
+        
+    success = graphene.Boolean()
+    
+    def mutate(self, info, id , title, content, category_id, **kwargs):
+        try:
+            existing_post = Post.objects.get(id=id)
+        except Post.DoesNotExist:
+            raise GraphQLError("Post not found")
+        
+        form = PostForm({"title": title, "content": content, "category": category_id}, instance=existing_post)
+        if form.is_valid():
+            updated_post = form.save()
+            return UpdatePost(success=True)
+        print(form.errors)
+        return UpdatePost(success=False)
+        
+    
+    
